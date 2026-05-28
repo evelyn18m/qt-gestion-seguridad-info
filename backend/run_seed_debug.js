8cab0a362b167403f6781b93097b31e5180c9f4c
Bryan Velastegui <bryanslvr@gmail.com>
2026-05-28 01:00:55 -0500

    chore(backend): remove temporary dev and debug scripts

--- backend/run_seed_debug.js
+++
@@ -1,7 +0,0 @@
-const { exec } = require('child_process');
-const fs = require('fs');
-exec('npx ts-node --transpile-only prisma/seed.ts', (error, stdout, stderr) => {
-    const log = `STDOUT:\n${stdout}\n\nSTDERR:\n${stderr}\n\nERROR:\n${error ? error.message : 'None'}`;
-    fs.writeFileSync('seed_debug_log.txt', log, 'utf8');
-    console.log('Log written to seed_debug_log.txt');
-});
