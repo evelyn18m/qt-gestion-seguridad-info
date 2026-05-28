9c8d978a11a16fbe46edd3808cf63fb4ee2be03a
Evelyn Morocho <velyn18morocho@gmail.com>
2026-05-27 19:18:54 -0500

    Refactor code structure for improved readability and maintainability

--- backend/dist/src/catalogos/dto/update-catalogo.dto.d.ts
+++
@@ -1,3 +0,0 @@
-import { CreateCatalogoDto } from './create-catalogo.dto';
-export declare class UpdateCatalogoDto extends CreateCatalogoDto {
-}
--- backend/dist/src/valoraciones/dto/update-valoracion.dto.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateValoracionDto = void 0;
-const create_valoracion_dto_1 = require("./create-valoracion.dto");
-class UpdateValoracionDto extends create_valoracion_dto_1.CreateValoracionDto {
-}
-exports.UpdateValoracionDto = UpdateValoracionDto;
-//# sourceMappingURL=update-valoracion.dto.js.map
