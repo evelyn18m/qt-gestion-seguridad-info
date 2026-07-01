d11dcbb59aa47d8812d528a51115a8664001c468
Evelyn <velyn18morocho@gmail.com>
2026-07-01 12:40:09 -0500

    Catalogo/TipoActivo-Actualizado

--- backend/src/catalogos/dto/create-catalogo.dto.ts
+++ backend/src/catalogos/dto/create-catalogo.dto.ts
@@ -1,15 +1,55 @@
+import { IsOptional, IsString, IsNumber } from 'class-validator';
+
 export class CreateCatalogoDto {
+  @IsOptional()
+  @IsString()
   nombre?: string;
+
+  @IsOptional()
+  @IsString()
   categoria?: string;
+
+  @IsOptional()
+  @IsString()
   descripcion?: string;
+
+  @IsOptional()
+  @IsString()
   detalle?: string;
+
+  @IsOptional()
+  @IsString()
   tipo?: string;
+
+  @IsOptional()
+  @IsString()
   nivel?: string;
+
+  @IsOptional()
+  @IsNumber()
   valor?: number;
+
+  @IsOptional()
+  @IsString()
   criterio?: string;
+
+  @IsOptional()
+  @IsString()
   tipoFuente?: string;
+
+  @IsOptional()
+  @IsNumber()
   macroProcesoId?: number;
+
+  @IsOptional()
+  @IsString()
   codigo?: string;
+
+  @IsOptional()
+  @IsString()
   seccion?: string;
+
+  @IsOptional()
+  @IsNumber()
   categoriaId?: number;
 }
